0a8fde3de985c6e7c27ccab1ea93f7c8d79195d4
CristianPro2002 <90258882+CristianPro2002@users.noreply.github.com>
2025-02-25 11:24:33 -0500

    Add files via upload

--- app/controllers/traduction.controller.js
+++ app/controllers/traduction.controller.js
@@ -16,6 +16,18 @@ const getAccounts = async (req, res, next) => {
   }
 };
 
+const updateAccount = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { created } = req.body;
+    const responseData = await ProductsModel.updateAccount(id, { created });
+    response(res, 200, responseData);
+  } catch (error) {
+    return resError(res, 500, error.message);
+  }
+};
+
 module.exports = {
   getAccounts,
+  updateAccount
 };
--- app/v1/routes/accounts.js
+++ app/v1/routes/accounts.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getAccounts} = require("../../controllers/traduction.controller");
+const { getAccounts, updateAccount} = require("../../controllers/traduction.controller");
 const { upload } = require("../../middleware/uploadFile");
 const router = express.Router();
 
 router.get("/", getAccounts);
+
+router.put("/:id", updateAccount);
 
 
 
